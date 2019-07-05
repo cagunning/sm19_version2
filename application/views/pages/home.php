42975cd9ff908d1ce29a411281616850314625ed
cagunning <46728544+cagunning@users.noreply.github.com>
2019-07-05 15:33:03 -0700

    Add files via upload

---
+++ application/views/pages/home.php
@@ -0,0 +1,7 @@
+<?php
+//application/views/pages/home.php
+?>
+
+<h1>Welcome to my home page</h1>
+<p>Any slugs find here will be wel behaved!</p>
+<p>IF you find them in the bathroom, please knock before entering! </p>
